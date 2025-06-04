57ec322fcb8803ad7651d5b33d0166f8dabac97a
Manuel Mera <manuel.mera@uma.es>
2025-06-04 15:01:30 +0200

    constructor added to Jarra class FIXED

--- src/main/java/jarras/Jarra.java
+++ src/main/java/jarras/Jarra.java
@@ -1,4 +1,17 @@
 package jarras;
 
 public class Jarra {
+
+    private final double capacidad;
+    private double contenido;
+
+    // CONSTRUCTOR
+    public Jarra(int capacidad){
+        if (capacidad <= 0){
+            throw new RuntimeException("La capacidad de la Jarra debe ser POSITIVA (MAYOR QUE 0)");
+        }
+        this.capacidad = capacidad;
+        this.contenido = 0;
+    }
+
 }
