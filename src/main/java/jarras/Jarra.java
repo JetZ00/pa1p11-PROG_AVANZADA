f46dc1385fd3733f86ce9b78d56b4424e28114e4
Manuel Mera <manuel.mera@uma.es>
2025-06-04 15:37:14 +0200

    Jarras Modifiable methods added to JARRA class

--- src/main/java/jarras/Jarra.java
+++ src/main/java/jarras/Jarra.java
@@ -23,14 +23,35 @@ public class Jarra {
         return contenido;
     }
 
-    // SETTERS (ALT+INSERT)
-    public void llena(double contenido) {
+    public void llena() {
         this.contenido = capacidad;
     }
 
-    public void vacia(double contenido) {
+    public void vacia() {
         this.contenido = 0;
     }
+
+    public void llenaDesde(Jarra otra){
+        if (this == otra) {
+            throw new RuntimeException("No se puede transferir de una Jarra hacia si misma");
+        }
+
+        /*
+         Comparamos que cantidad se procede a traspasar, es decir:
+         ¿Cual de las dos es la cantidad a mover?
+
+         Pues se procederá o bien mediante el espacio que queda en THIS, o bien con lo que tiene OTRA
+         Sea cual sea de las dos, traspasará o bien, el maximo permitido en THIS
+                                             o bien, el maximo que puede traspasar OTRA
+         */
+
+        double espacioDisponible = this.capacidad - this.contenido;
+        double cantidadATransvasar = Math.min(espacioDisponible,otra.contenido);
+
+        this.contenido += cantidadATransvasar;
+        otra.contenido -= cantidadATransvasar;
+    }
+
 
 
 
