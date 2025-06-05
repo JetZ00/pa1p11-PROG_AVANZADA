feb31a97ea223621844b23ce081f5711b85870e5
Manuel Mera <manuel.mera@uma.es>
2025-06-05 16:31:56 +0200

    Constructors fixed for Mesa Class

--- src/main/java/jarras/Mesa.java
+++ src/main/java/jarras/Mesa.java
@@ -1,7 +1,26 @@
 package jarras;
 
 public class Mesa {
+    /*public enum Posicion {
+        Izquierda,
+        Derecha
+    }*/
 
+    // CONSTRUCTORES
 
-    public Mesa ()
+    private Jarra jarraIzq, jarraDer;
+
+    public Mesa (Jarra jarraIzq, Jarra jarraDer){
+        if (jarraIzq==jarraDer){
+            throw new RuntimeException("No pueden ser la misma Jarra");
+        }
+        this.jarraIzq = jarraIzq;
+        this.jarraDer = jarraDer;
+    }
+
+    public Mesa (int cIzq, int cDer ){
+        this.jarraIzq = new Jarra(cIzq);
+        this.jarraDer = new Jarra(cDer);
+    }
+
 }
