02a369aed8d79f5eb383c9fc7484bd74364686d0
Manuel Mera <manuel.mera@uma.es>
2025-06-06 21:56:22 +0200

    EjemploUsoJarras2 added to Main Class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,34 @@
 package org.example;
 import jarras.Jarra;
+import static jarras.Mesa.Posicion.*;
 
 public class Main {
     public static void main(String[] args) {
         Jarra jarraA = new Jarra(7);
-        Jarra jarraB = new Jarra(4);
+        Jarra jarraB = new Jarra(5);
 
-        jarraA.llena();
+        jarraB.llena();
         System.out.print(jarraA + ", "+jarraB + "\n");
 
-        jarraB.llenaDesde(jarraA);
+        jarraA.llenaDesde(jarraB);
         System.out.print(jarraA + ", "+jarraB + "\n");
 
-        jarraB.vacia();
+        jarraB.llena();
         System.out.print(jarraA + ", "+jarraB + "\n");
 
-        jarraB.llenaDesde(jarraA);
+        jarraA.llenaDesde(jarraB);
+        System.out.print(jarraA + ", "+jarraB + "\n");
+
+        jarraA.vacia();
+        System.out.print(jarraA + ", "+jarraB + "\n");
+
+        jarraA.llenaDesde(jarraB);
+        System.out.print(jarraA + ", "+jarraB + "\n");
+
+        jarraB.llena();
+        System.out.print(jarraA + ", "+jarraB + "\n");
+
+        jarraA.llenaDesde(jarraB);
         System.out.print(jarraA + ", "+jarraB + "\n");
     }
 }
